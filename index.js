d4ac066f614f1ee3bd4a8b34e65ca1b586cc450f
root <root@DESKTOP-598GE48.localdomain>
2023-10-05 15:11:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+
+// Function to calculate distance from headquarters in blocks
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks - 42);
+  }
+  
+  // Function to calculate distance from headquarters in feet
+  function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264; // Assuming 1 block is 264 feet
+  }
+  
+  // Function to calculate distance traveled in feet
+  function distanceTravelledInFeet(startBlock, endBlock) {
+    return Math.abs(startBlock - endBlock) * 264; // Assuming 1 block is 264 feet
+  }
+  
+  // Function to calculate fare price
+  function calculatesFarePrice(startBlock, endBlock) {
+    const distance = distanceTravelledInFeet(startBlock, endBlock);
+  
+    if (distance <= 400) {
+      return 0; // Free sample
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02; // 2 cents per foot after the first 400 feet
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25; // $25 for a distance over 2000 feet
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+  
